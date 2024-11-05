32ccf286629d6cf2a7c8425dcdd2597b42661713
gubarev1970 <gubarev1970@seznam.cz>
2024-11-05 17:09:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,46 +1,31 @@
-﻿const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt');
-const { Pool } = require('pg'); // Nebo jiná knihovna pro vaši databázi
-const path = require('path'); // Přidáno pro zpracování cest
+const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
+const port = process.env.PORT || 10000;
 
-app.use(bodyParser.json()); // Pro zpracování JSON dat
-app.use(express.static('public')); // Tato složka obsahuje vaše statické soubory
-
-// Připojení k databázi
 const pool = new Pool({
-    connectionString: process.env.db_url // nebo jiná cesta k databázi
+  user: 'postgres',
+  host: 'localhost',
+  database: 'db_url',
+  password: 'Charalamba11@', // zde vlož své heslo
+  port: 5432,
 });
 
-// Endpoint pro kořenovou cestu
+
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'public','index.html')); // Ujistěte se, že odkazujete na správnou cestu
+  res.sendFile(__dirname + '/index.html');
 });
 
-// Endpoint pro registraci
-app.post('/register', async (req, res) => {
-    const { email, username, password, birthdate, height, weight, eyes, hair, children, job, hobbies, orientation, search } = req.body;
-
-    try {
-        // Hashování hesla
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Vložení uživatelských dat do databáze
-        const result = await pool.query(
-            'INSERT INTO users (email, username, password, birthdate, height, weight, eyes, hair, children, job, hobbies, orientation, search) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
-            [email, username, hashedPassword, birthdate, height, weight, eyes, hair, children, job, hobbies, orientation, search]
-        );
-
-        res.json({ success: true });
-    } catch (error) {
-        console.error('Chyba při registraci:', error);
-        res.json({ success: false, message: 'Chyba při registraci. Zkontrolujte, zda uživatelské jméno nebo e-mail již neexistují.' });
-    }
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
-
-// Spuštění serveru
-app.listen(3000, () => {
-    console.log('Server běží na portu 3000');
+app.get('/users', async (req, res) => {
+  try {
+    const users = await db.collection('users').find({}).toArray();
+    res.json(users);
+  } catch (error) {
+    res.status(500).send("Chyba při načítání uživatelů");
+  }
 });
-
