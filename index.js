131d09f739d2ce7ef6a8cb5d52f4051f6b9fc410
gubarev1970 <gubarev1970@seznam.cz>
2024-11-06 15:37:38 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 10000;
 
+// Servírování statických souborů ze složky 'public'
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pro hlavní stránku
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Spuštění serveru
 app.listen(PORT, () => {
     console.log(`Server běží na portu ${PORT}`);
 });
