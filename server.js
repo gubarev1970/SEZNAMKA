0e75a1c94f6b411a6bdb3e13601ec085265e28ba
gubarev1970 <gubarev1970@seznam.cz>
2024-11-09 17:28:27 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -7,28 +7,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Rucní nastavení cest k jednotlivým stránkám v korenovém adresári
-app.get('/registrace.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'registrace.html'));
-});
-app.get('/prihlášení.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'prihlášení.html'));
-});
-app.get('/profil.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'profil.html'));
-});
-app.get('/chat.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'chat.html'));
-});
-app.get('/koupit mince.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'koupit mince.html'));
-});
-app.get('/komentáre.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'komentáre.html'));
-});
-app.get('/fotky.html', (req, res) => {
-    res.sendFile(path.join(__dirname, 'fotky.html'));
-});
 
 // Nastavení portu
 const PORT = process.env.PORT || 10000;
