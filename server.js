cbf375d4bde2867fc4906bb276ba6457b68ac88e
gubarev1970 <gubarev1970@seznam.cz>
2024-11-09 17:37:51 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -2,11 +2,36 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Nastavení hlavní stránky na `index.html` v korenovém adresári
+// Nastavení složky pro statické soubory na složku public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Nastavení hlavní stránky na `index.html` ve složce public
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Rucní nastavení cest k jednotlivým stránkám v složce public
+app.get('/registrace.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'registrace.html'));
+});
+app.get('/prihlášení.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'prihlášení.html'));
+});
+app.get('/profil.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'profil.html'));
+});
+app.get('/chat.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+});
+app.get('/koupit mince.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'koupit mince.html'));
+});
+app.get('/komentáre.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'komentáre.html'));
+});
+app.get('/fotky.html', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'fotky.html'));
+});
 
 // Nastavení portu
 const PORT = process.env.PORT || 10000;
